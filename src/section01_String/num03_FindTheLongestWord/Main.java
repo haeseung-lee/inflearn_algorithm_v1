5751b03e72ab93b32b7c3177bf8581e728a2f036
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-07 06:24:55 +0900

    25.05.07 문장속 단어(한개의 문장 속 가장 긴 단어를 출력)_solution2

--- src/section01_String/num03_FindTheLongestWord/Main.java
+++ src/section01_String/num03_FindTheLongestWord/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String str = in.nextLine();
 
         Main m = new Main();
-        System.out.println(m.solution(str));
+        System.out.println(m.solution2(str));
 
         in.close();
     }
@@ -22,6 +22,20 @@ public class Main {
             String temp = strArr[i];
             if(answer.length() < temp.length()){
                 answer = temp;
+            }
+        }
+        return answer;
+    }
+
+    String solution2(String str){
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+
+        for(String s : str.split(" ")){
+            int len = s.length();
+            if(m < len){
+                m = len;
+                answer = s;
             }
         }
         return answer;
