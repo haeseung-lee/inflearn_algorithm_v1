099cd7523f4a276c2f870c1142483a88d81f213f
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-13 06:17:54 +0900

    아스키코드 활용

--- src/section01_String/num09_ExtractNumbers/Main.java
+++ src/section01_String/num09_ExtractNumbers/Main.java
@@ -17,4 +17,17 @@ public class Main {
         str = str.replaceAll("[^0-9]", "");
         return Integer.parseInt(str);
     }
+
+    //0~9 : x >= 48 && x<=57
+    private int solution2(String str){
+
+        int answer = 0;
+
+        for(char c : str.toCharArray()){
+            if(c >= 48 && c <= 57){
+                answer = answer*10 + (c -48);
+            }
+        }
+        return answer;
+    }
 }
