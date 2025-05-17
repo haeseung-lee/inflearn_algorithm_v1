56ef99036dab4f0a8e4c5ba753c4b8f792b55e05
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-17 09:31:21 +0900

    ArrayList 활용

--- src/section02_Array/num01_PrintLargerNumber/Main.java
+++ src/section02_Array/num01_PrintLargerNumber/Main.java
@@ -1,17 +1,30 @@
 package section02_Array.num01_PrintLargerNumber;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     //자신의 바로 앞 수보다 큰 수만 출력
     public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        String cnt = in.nextLine();
-        String str = in.nextLine();
-
         Main m = new Main();
-        System.out.println(m.solution(cnt, str));
+//        Scanner in=new Scanner(System.in);
+//        String cnt = in.nextLine();
+//        String str = in.nextLine();
+//
+//        System.out.println(m.solution(cnt, str));
 
+        //solution2.
+        Scanner in=new Scanner(System.in);
+        int cnt = in.nextInt();
+        int[] arr  = new int[cnt];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for(int n : m.solution2(cnt, arr)){
+            System.out.print(n + " ");
+        }
     }
 
     private String solution(String cnt, String str){
@@ -28,5 +41,17 @@ public class Main {
             }
         };
         return answer.toString();
+    }
+
+    private ArrayList<Integer> solution2(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+
+        for(int i = 1; i < n; i++){
+            if(arr[i-1] < arr[i]){
+                answer.add(arr[i]);
+            }
+        }
+        return answer;
     }
 }
