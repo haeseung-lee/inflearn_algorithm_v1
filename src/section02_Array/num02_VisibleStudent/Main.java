97968ead1040b4dc5ca85ea364c9d4d3a367a9c1
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-18 07:48:22 +0900

    변수명 수정

--- src/section02_Array/num02_VisibleStudent/Main.java
+++ src/section02_Array/num02_VisibleStudent/Main.java
@@ -19,12 +19,12 @@ public class Main {
 
     private int solution(int[] arr) {
         int answer = 1;
-        int min = arr[0];
+        int max = arr[0];
 
         for(int i = 1; i < arr.length; i ++){
-            if(min < arr[i]){
+            if(max < arr[i]){
                 answer++;
-                min = arr[i];
+                max = arr[i];
             }
         }
         return answer;
