21daa2e9fe0c62ea179a38b7d5baaf13796d0b02
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-22 04:24:43 +0900

    뒤집었을 때 소수 찾기 solution2

--- src/section02_Array/num06_ReversePrimeNumbers/Main.java
+++ src/section02_Array/num06_ReversePrimeNumbers/Main.java
@@ -7,17 +7,25 @@ import java.util.Scanner;
 public class Main {
     //뒤집었을 때 소수 찾기
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int n = in.nextInt();
-
-        String[] sArr = new String[n];
-        for(int i=0; i<n; i++){
-            sArr[i] = in.next();
-        }
-
         Main m = new Main();
+//        Scanner in=new Scanner(System.in);
+//        int n = in.nextInt();
+//
+//        String[] sArr = new String[n];
+//        for(int i=0; i<n; i++){
+//            sArr[i] = in.next();
+//        }
+//        for(int i : m.solution(n, sArr)){
+//            System.out.print(i + " ");
+//        }
 
-        for(int i : m.solution(n, sArr)){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i : m.solution2(n, arr)){
             System.out.print(i + " ");
         }
     }
@@ -63,5 +71,34 @@ public class Main {
             }
         }
         return answer;
+    }
+    private ArrayList<Integer> solution2(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        //수 뒤집기
+        for(int i=0; i<n; i++){
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0){
+                int t = tmp%10;
+                res = res * 10 + t;
+                tmp = tmp/10;
+            }
+            if(isPrime(res)){
+                answer.add(res);
+            }
+        }
+        return answer;
+    }
+
+    private boolean isPrime(int num){
+        if(num == 1){
+            return false;
+        }
+        for(int i=2; i<num; i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
