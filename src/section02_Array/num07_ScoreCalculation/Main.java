85c1a2ac9312b5f0b9a0058523087df8a019a05c
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-23 06:27:58 +0900

    점수 계산

---
+++ src/section02_Array/num07_ScoreCalculation/Main.java
@@ -0,0 +1,33 @@
+package section02_Array.num07_ScoreCalculation;
+
+import java.util.Scanner;
+
+public class Main {
+    //점수 계산 - 정답 1점, 오답 0점, 연속 정답 가산점+1;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = in.nextInt();
+        }
+
+        Main m = new Main();
+        System.out.println(m.solution(n, arr));
+    }
+
+    private int solution(int n, int[] arr) {
+        int answer = 0;
+        int consecutive = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                consecutive++;
+            } else {
+                consecutive = 0;
+            }
+            answer += consecutive;
+        }
+        return answer;
+    }
+}
