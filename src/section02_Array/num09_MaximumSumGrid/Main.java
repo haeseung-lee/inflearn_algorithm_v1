fd7bd421aecc679f70266f88d2abb51b7b85d734
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-25 07:27:57 +0900

    격자판 최대합

---
+++ src/section02_Array/num09_MaximumSumGrid/Main.java
@@ -0,0 +1,47 @@
+package section02_Array.num09_MaximumSumGrid;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[][] grid = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                grid[i][j] = in.nextInt();
+            }
+        }
+        Main m = new Main();
+        System.out.println(m.solution(n, grid));
+    }
+
+    private int solution(int n, int[][] grid) {
+        int answer = 0;
+        int sum1 = 0; //가로합
+        int sum2 = 0; //세로합
+        int sum3 = 0; //대각선 합
+        int sum4 = 0; //대각석 역순의 합
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0 ; j < n; j++){
+                sum1 += grid[i][j];
+                sum2 += grid[j][i];
+            }
+            sum3 += grid[i][i];
+            sum4 += grid[n-1-i][n-1-i];
+
+            if(answer < sum1 || answer < sum2){
+                answer = Math.max(sum1, sum2);
+            }
+            sum1 = 0;
+            sum2 = 0;
+        }
+        if(answer < sum3 || answer < sum4){
+            answer = Math.max(sum3, sum4);
+        }
+        return answer;
+    }
+}
