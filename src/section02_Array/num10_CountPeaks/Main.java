ca6206538c2373fa4226b314adf8dfb42d18c16c
Haeseung Lee <dlgotmd93@gmail.com>
2025-05-26 07:03:10 +0900

    봉우리 - 좌표를 활용

--- src/section02_Array/num10_CountPeaks/Main.java
+++ src/section02_Array/num10_CountPeaks/Main.java
@@ -6,15 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        int[][] arr = new int[n+2][n+2];
-        for(int i = 1; i < arr[i].length-1; i++){
-            for(int j = 1; j < arr[j].length-1; j++){
+//        int[][] arr = new int[n+2][n+2];
+//        for(int i = 1; i < arr[i].length-1; i++){
+//            for(int j = 1; j < arr[j].length-1; j++){
+//                arr[i][j] = in.nextInt();
+//            }
+//        }
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 arr[i][j] = in.nextInt();
             }
         }
 
         Main m = new Main();
-        System.out.println(m.solution(n, arr));
+        //System.out.println(m.solution(n, arr));
+        System.out.println(m.solution2(n, arr));
     }
 
     private int solution(int n, int[][] arr) {
@@ -37,6 +44,29 @@ public class Main {
 
             }
         }
+        return answer;
+    }
+
+    int[] dx={-1, 0, 1, 0};
+    int[] dy={0,1,0,-1};
+
+    private int solution2(int n, int[][] arr){
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                boolean flag = true;
+                for(int k = 0; k < 4; k++){
+                    int nx = j + dx[k];
+                    int ny = i + dy[k];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[ny][nx] >= arr[i][j]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) answer++;
+            }
+        }
+
         return answer;
     }
 }
