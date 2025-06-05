2e3d3763015b6acf714ae89d05241eef35a0e1f6
Haeseung Lee <dlgotmd93@gmail.com>
2025-06-06 00:39:25 +0900

    Anagram - 방법2

--- src/section04_HashMapTreeSet/num02_Anagram/Main.java
+++ src/section04_HashMapTreeSet/num02_Anagram/Main.java
@@ -9,9 +9,9 @@ public class Main {
         Scanner in=new Scanner(System.in);
         String s1 = in.nextLine();
         String s2 = in.nextLine();
-        
+
         Main main = new Main();
-        System.out.println(main.solution(s1, s2));
+        System.out.println(main.solution2(s1, s2));
     }
 
     private String solution(String s1, String s2) {
@@ -29,6 +29,25 @@ public class Main {
             if(cnt == 0 || cnt != map1.get(key)){
                 return "NO";
             }
+        }
+        return "YES";
+    }
+
+    /*
+    * 처음 문자열로 hashMap 구성(k:문자, v:갯수)
+    * 두번째 문자열로 hashMap 에 있는 같은 문자의 갯수 줄이기
+    * 같은 문자가 없거나 더 줄일 수 없을 때 NO를 return
+    * */
+    private String solution2(String s1, String s2){
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s1.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        for(char c : s2.toCharArray()){
+            if(!map.containsKey(c) || map.get(c) == 0){
+                return "NO";
+            }
+            map.put(c, map.get(c) - 1);
         }
         return "YES";
     }
