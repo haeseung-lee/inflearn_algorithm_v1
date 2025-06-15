84e38422d9ceef342c2c27967c7255b2e61a5d70
Haeseung Lee <dlgotmd93@gmail.com>
2025-06-16 06:09:52 +0900

    공주 구하기 - 방법2

--- src/section05_StackAndQueue/num06_SavingThePrincess/Main.java
+++ src/section05_StackAndQueue/num06_SavingThePrincess/Main.java
@@ -34,4 +34,22 @@ public class Main {
         answer = que.poll();
         return answer;
     }
+
+    private int solution2(int n, int k){
+        int answer = 0;
+        Queue<Integer> Q = new LinkedList<>();
+        for(int i = 1; i <= n; i++){
+            Q.offer(i);
+        }
+        while (!Q.isEmpty()){
+            for(int i = 1; i < k; i++){
+                Q.offer(Q.poll());
+            }
+            Q.poll();
+            if(Q.size() == 1){
+                answer = Q.poll();
+            }
+        }
+        return answer;
+    }
 }
