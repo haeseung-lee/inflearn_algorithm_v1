138cc0319ad54d3fedd13485cb5c6b7693634272
Haeseung Lee <dlgotmd93@gmail.com>
2025-06-22 12:20:58 +0900

    Least Recently Used - 배열활용

--- src/section06_SortingAndSearching/num04_LeastRecentlyUsed/Main.java
+++ src/section06_SortingAndSearching/num04_LeastRecentlyUsed/Main.java
@@ -25,7 +25,7 @@ public class Main {
         }
         Main main = new Main();
 
-        for(int x : main.solution(s, n, arr)){
+        for(int x : main.solution2(s, n, arr)){
             System.out.print(x + " ");
         }
 
@@ -46,5 +46,29 @@ public class Main {
             }
         }
         return que;
+    }
+
+    private int[] solution2(int s, int n, int[] arr) {
+        int[] cache = new int[s];
+        for(int x : arr){
+            int pos = -1;
+            for(int i = 0; i < s; i++){
+                if(x == cache[i]){
+                    pos = i;
+                }
+            }
+            if(pos == -1){
+                for(int i = s-1; i >= 1; i--){
+                    cache[i] = cache[i-1];
+                }
+                cache[0] = x;
+            } else {
+                for(int i = pos; i >= 1; i--){
+                    cache[i] = cache[i-1];
+                }
+                cache[0] = x;
+            }
+        }
+        return cache;
     }
 }
